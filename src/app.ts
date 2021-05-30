901a3c92cf505ed35104bae6aa9d7f2b2e64a20c
lsh730 <525gus@naver.com>
2021-05-30 14:46:42 +0900

    3. feat : todo, note components

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
+import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
+import { TodoComponent } from './components/page/item/todo.js';
 class App {
     private readonly page: PageComponent;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot)
+
         const image = new ImageComponent('Image', 'http://picsum.photos/600/300', '이미지이미지');
         image.attachTo(appRoot, 'beforeend');
+
+        const note = new NoteComponent('Note', 'note body');
+        note.attachTo(appRoot, 'beforeend');
+
+        const todo = new TodoComponent('Todo', 'Todo body');
+        todo.attachTo(appRoot, 'beforeend');
     }
 }
 
--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,22 +1,22 @@
 import { BaseComponent } from '../../component.js';
 
 export class ImageComponent extends BaseComponent<HTMLElement> {
-    constructor(title: string, url: string, disc: string) {
+    constructor(title: string, url: string, body: string) {
         super(`<section class="image">
                 <div class="image__holder"><img src="" alt="" class="image__thumnail"></div>
-                <p class="image__title"></p>
-                <p class="image__disc"></p>
+                <h2 class="image__title"></h2>
+                <p class="image__body"></p>
             </section>`)
 
         const imageElement = this.element.querySelector('.image__thumnail')! as HTMLImageElement;
         imageElement.src = url;
         imageElement.alt = title;
 
-        const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+        const titleElement = this.element.querySelector('.image__title')! as HTMLHeadingElement;
         titleElement.textContent = title;
 
-        const discElement = this.element.querySelector('.image__disc')! as HTMLParagraphElement;
-        discElement.textContent = disc;
+        const bodyElement = this.element.querySelector('.image__body')! as HTMLParagraphElement;
+        bodyElement.textContent = body;
     }
 
 
