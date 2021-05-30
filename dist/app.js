f026ee4dc1f283ddc857f5f049cb3d8cf8137bac
lsh730 <525gus@naver.com>
2021-05-30 18:41:54 +0900

    6. refactor : PageComponent
    PageComponent 와 PageItemConponent 의 디커플링

--- dist/app.js
+++ dist/app.js
@@ -1,11 +1,11 @@
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent } from './components/page/page.js';
+import { PageComponent, PageItemComponent } from './components/page/page.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
 class App {
     constructor(appRoot) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
         const image = new ImageComponent('Image', 'http://picsum.photos/600/300', 'image body');
         this.page.addChild(image);
