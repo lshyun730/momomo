88ff2810a5f6651aaf642fe954d387905763ac9f
lsh730 <525gus@naver.com>
2021-05-30 15:17:55 +0900

    4. feat : video component

--- dist/app.js
+++ dist/app.js
@@ -2,16 +2,19 @@ import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
-        const image = new ImageComponent('Image', 'http://picsum.photos/600/300', '이미지이미지');
+        const image = new ImageComponent('Image', 'http://picsum.photos/600/300', 'image body');
         image.attachTo(appRoot, 'beforeend');
         const note = new NoteComponent('Note', 'note body');
         note.attachTo(appRoot, 'beforeend');
-        const todo = new TodoComponent('Todo', 'Todo body');
+        const todo = new TodoComponent('Todo', 'todo body');
         todo.attachTo(appRoot, 'beforeend');
+        const video = new VideoComponent('Video', 'https://www.youtube.com/embed/7pp3QuCqlhw', ' video body');
+        video.attachTo(appRoot, 'beforeend');
     }
 }
 new App(document.querySelector('.document'));
