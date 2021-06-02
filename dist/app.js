32e8d6876d83ffdaf82f8c3e7db89f8a8aabb10d
lsh730 <525gus@naver.com>
2021-06-02 12:18:34 +0900

    7. feat : dialog

--- dist/app.js
+++ dist/app.js
@@ -1,8 +1,9 @@
+import { PageComponent, PageItemComponent } from './components/page/page.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { PageComponent, PageItemComponent } from './components/page/page.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { InputDialog } from './components/dialog/dialog.js';
 class App {
     constructor(appRoot) {
         this.page = new PageComponent(PageItemComponent);
@@ -15,6 +16,17 @@ class App {
         this.page.addChild(todo);
         const video = new VideoComponent('Video', 'https://www.youtube.com/embed/7pp3QuCqlhw', ' video body');
         this.page.addChild(video);
+        const imageBtn = document.querySelector('#new-image');
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+            dialog.setOncloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnsubmitListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.attachTo(document.body);
+        });
     }
 }
 new App(document.querySelector('.document'));
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
+import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 import { Component } from './components/component.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
-import { Composable, PageComponent, PageItemComponent } from './components/page/page.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { InputDialog } from './components/dialog/dialog.js';
 
 class App {
     private readonly page: Component & Composable;
@@ -23,6 +24,20 @@ class App {
         const video = new VideoComponent('Video', 'https://www.youtube.com/embed/7pp3QuCqlhw', ' video body')
         this.page.addChild(video);
 
+        const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+
+            dialog.setOncloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnsubmitListener(() => {
+                //섹션을 만들어서 페이지에 추가
+                dialog.removeFrom(document.body);
+            });
+
+            dialog.attachTo(document.body);
+        })
     }
 }
 
